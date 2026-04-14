0e9cbe636b75c66880f6d46eb0058ed67a8e56e4
Shlesha Uppala <147795373+Shlesha5847@users.noreply.github.com>
2026-04-15 00:50:05 +0530

    CHANGE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,8 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
