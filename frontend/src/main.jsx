2be4826a9c61e913f5c647d832237c2c524c21e5
Shlesha Uppala <147795373+Shlesha5847@users.noreply.github.com>
2026-04-14 20:02:52 +0530

    Integrate Google OAuth provider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId=" 649706721942-j41h5dhifq073fn9u40eop7g9pfaiprv.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
